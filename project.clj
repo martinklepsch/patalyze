6d08ce958d6236ef33fd9bcfd11b820a558814f5
Martin Klepsch <martinklepsch@gmail.com>
2014-04-18 11:40:41 +0200

    start implementing some dispatching based on the version of the xml-str

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/core.match "0.2.1"]
                  [clojurewerkz/elastisch "2.0.0-beta4"]])
--- src/patalyze/core.clj
+++ src/patalyze/core.clj
@@ -3,6 +3,7 @@
             [clojure.zip :as zip]
             [clojure.data.zip :as dzip]
             [clojure.data.zip.xml :as zf]
+            [clojure.core.match :refer (match)]
             [clojurewerkz.elastisch.rest          :as esr]
             [clojurewerkz.elastisch.rest.index    :as esi]
             [clojurewerkz.elastisch.query         :as q]
@@ -29,7 +30,7 @@
   "Find all xmls in the resources/patent_archives/ directory"
   (let [directory (clojure.java.io/file "resources/applications/")
         files (file-seq directory)]
-     (filter (fn [f] (re-find #"ipab.*\.xml" f)) (map str files))))
+     (filter (fn [f] (re-find #"pab.*\.xml" f)) (map str files))))
 
 (defn split-file [file]
   "Splits archive file into strings at xml statements"
@@ -67,12 +68,20 @@
              zf/text))
 
 ; INVENTORS
-(defn inventors [xml-zipper]
-  (zf/xml-> xml-zipper
-            :us-bibliographic-data-application
-            :us-parties ;; :parties
-            :inventors  ;; :applicants
-            dzip/children-auto))
+(defn inventors [version xml-zipper]
+  (cond
+    (= version :v43)
+    (zf/xml-> xml-zipper
+              :us-bibliographic-data-application
+              :us-parties ;; :parties
+              :inventors  ;; :applicants
+              dzip/children-auto)
+    (= version :v42)
+    (zf/xml-> xml-zipper
+              :us-bibliographic-data-application
+              :parties
+              :applicants
+              dzip/children-auto)))
 
 (defn inventor->map [inventor-node]
   (str (zf/xml1-> inventor-node :addressbook :first-name zf/text) " "
@@ -95,16 +104,33 @@
    :role (zf/xml1-> assignee-node :role zf/text)})
 
 ; PUTTING IT TOGETHER
-(defn patentxml->map [xml-zipper]
-  {:assignees (map assignee->map (assignees xml-zipper))
-   :inventors (map inventor->map (inventors xml-zipper))
-   :abstract (invention-abstract xml-zipper)
-   :title (invention-title xml-zipper)
-   :uid (publication-identifier xml-zipper)})
+(defn patentxml->map [xml-str]
+  (let [version    (detect-version xml-str)
+        xml-zipper (str->zipper xml-str)]
+    {:assignees (map assignee->map (assignees xml-zipper))
+     :inventors (map inventor->map (inventors version xml-zipper))
+     :abstract (invention-abstract xml-zipper)
+     :title (invention-title xml-zipper)
+     :uid (publication-identifier xml-zipper)}))
 
 (defn read-file [xml-archive]
   "Reads one weeks patent archive and returns a seq of maps w/ results"
-  (map patentxml->map (map str->zipper (split-file xml-archive))))
+  (map patentxml->map (split-file xml-archive)))
+
+(defn union-re-patterns [& patterns]
+  (re-pattern (apply str (interpose "|" (map #(str "(?:" % ")") patterns)))))
+
+(def dtd-matcher
+  (union-re-patterns #"us-patent-application-v4\d{1}-\d{4}-\d{2}-\d{2}\.dtd"
+                     #"pap-v\d{2}-\d{4}-\d{2}-\d{2}\.dtd"))
+
+(defn detect-version [xml-str]
+  (match [(apply str (re-seq dtd-matcher xml-str))]
+     ["us-patent-application-v43-2012-12-04.dtd"] :v43
+     ["us-patent-application-v42-2006-08-23.dtd"] :v42
+     ["us-patent-application-v40-2004-12-02.dtd"] :v40
+     ["pap-v15-2001-01-31.dtd"]                   :v15
+     :else :not-recognized))
 
 ; ELASTISCH
 (def cmapping
@@ -128,14 +154,12 @@
 (defn index-file [f]
   (bulk-insert (prepare-bulk-op (read-file f))))
 
-(index-file (first (patent-application-files)))
-
 (comment
   (esr/connect! "http://127.0.0.1:9200")
   (esd/delete-by-query-across-all-indexes-and-types (q/match-all))
   ;; creates an index with default settings and no custom mapping types
-(time (index-file (nth (patent-application-files) 4)))
-(esd/count "patalyze_development" "patent" (q/match-all))
+  (time (index-file (nth (patent-application-files) 4)))
+  (esd/count "patalyze_development" "patent" (q/match-all))
 
   (esd/count "patalyze_development" "patent" (q/match-all))
   (esi/delete "patalyze_development")
