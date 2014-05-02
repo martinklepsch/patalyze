80f48df92631107856c6a1593d15305c4a2274fe
Martin Klepsch <martinklepsch@gmail.com>
2014-05-02 18:42:53 +0200

    add schema + kibit

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/core.match "0.2.1"]
-                 [clojurewerkz/elastisch "2.0.0-beta4"]])
+                 [prismatic/schema "0.2.1"]
+                 [clojurewerkz/elastisch "2.0.0-beta4"]]
+  :plugins      [[lein-kibit "0.0.8"]])
