8e27b2d8b0974b383c307fbffb5a6a6e393aa68c
Martin Klepsch <martinklepsch@gmail.com>
2014-05-05 11:56:03 +0200

    add some older tests that might be useful later

--- test/patalyze/core_test.clj
+++ test/patalyze/core_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [patalyze.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(clojure.test/run-tests)
+
+(def inventors
+  {:v15 ["Lawrence A. Clark" "James L. Priest"]
+   :v43 ["Daniel Francis Lowery" "Jon M. Malinoski"]})
+
+(deftest inventors-test
+  (testing "inventors v15"
+    (is (= (:inventors (patentxml->map (:v15 (version-samples))))
+          '("Lawrence A. Clark" "James L. Priest"))))
+
+  (testing "Inventors v43"
+    (is (= (:inventors (patentxml->map (:v43 (version-samples))))
+          '("Daniel Francis Lowery" "Jon M. Malinoski")))))
+;;           '("David C. Holland")
+;;           '("Tina Goldkind")
+;;           '("Andrea Tomann" "Ken Zemach" "Bill McDonough" "Tim Smith")
+;;           '("Philip A. Eckhoff" "Roderick A. Hyde" "Muriel Y. Ishikawa" "Jordin T. Kare" "Lowell L. Wood, JR.")
+;;           '("Daniel Francis Lowery" "Jon M. Malinoski"))))))
