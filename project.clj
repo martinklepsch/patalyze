a756e390319ed8841300589fa36ff71c144d54dd
Martin Klepsch <martinklepsch@gmail.com>
2014-05-09 00:42:51 +0200

    update lighttable-nrepl stuff

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [org.clojure/tools.nrepl "0.2.3"]
                  [enlive "1.1.5"]
                  [clojurewerkz/elastisch "2.0.0-beta4"]
-                 [lein-light-nrepl "0.0.4"]]
+                 [lein-light-nrepl "0.0.17"]]
   :plugins      [[lein-kibit "0.0.8"]]
 
   :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
