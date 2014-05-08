0c4f8e02d8c1eab96f74500a703f40738281f063
Martin Klepsch <martinklepsch@gmail.com>
2014-05-09 00:02:01 +0200

    add lighttable nrepl middleware to nrepl server started in -main

--- src/patalyze/nrepl.clj
+++ src/patalyze/nrepl.clj
@@ -1,8 +1,8 @@
 (ns patalyze.nrepl
-  (:require [clojure.tools.nrepl.server :refer (start-server stop-server)])
+  (:require [clojure.tools.nrepl.server :refer (start-server stop-server default-handler)])
   (:gen-class :main true))
 
 (defn -main
   "The application's main function"
   [& args]
-  (start-server :bind "127.0.0.1" :port 42042))
+  (start-server :bind "127.0.0.1" :port 42042 :handler (default-handler [lighttable.nrepl.handler/lighttable-ops])))
