ba9a36556bf2b4c9930c07c948605cd54d0443de
Martin Klepsch <martinklepsch@gmail.com>
2014-05-09 12:43:51 +0200

    temporarily disable lighttable nrepl stuff

--- src/patalyze/nrepl.clj
+++ src/patalyze/nrepl.clj
@@ -1,9 +1,9 @@
 (ns patalyze.nrepl
-  (:require [clojure.tools.nrepl.server :refer (start-server stop-server default-handler)]
-            [lighttable.nrepl.handler :refer (lighttable-ops)])
+  (:require [clojure.tools.nrepl.server :refer (start-server stop-server default-handler)])
+            ; [lighttable.nrepl.handler :refer (lighttable-ops)])
   (:gen-class :main true))
 
 (defn -main
   "The application's main function"
   [& _]
-  (start-server :bind "127.0.0.1" :port 42042 :handler (default-handler lighttable-ops)))
+  (start-server :bind "127.0.0.1" :port 42042)); :handler (default-handler lighttable-ops)))
