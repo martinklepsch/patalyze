db255bc88e3c558c9547b87b5ae391aa62415f45
Martin Klepsch <martinklepsch@googlemail.com>
2014-06-30 13:42:42 +0200

    add some routes to query apple patents

--- src/patalyze/core.clj
+++ src/patalyze/core.clj
@@ -1,5 +1,6 @@
 (ns patalyze.core
   (:require [clojure.tools.nrepl.server :as nrepl]
+            [patalyze.index             :as index]
             [liberator.core             :refer  [resource defresource]]
             [ring.middleware.params     :refer  [wrap-params]]
             [ring.middleware.reload     :refer  [wrap-reload]]
@@ -9,11 +10,28 @@
             [taoensso.timbre            :as timbre :refer [log  trace  debug  info  warn  error]])
   (:gen-class))
 
+
+(defn simplify-maps [ms]
+  (map
+    (fn [m]
+      (update-in m [:inventors] #(clojure.string/join ", " %)))
+    ms))
+
+(defresource apple [pub-date]
+  :available-media-types  ["text/csv" "application/json"]
+  :handle-ok  (fn  [_] (simplify-maps (index/patents-by-org-and-date "Apple Inc." pub-date))))
+
+(defresource stats []
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (index/patent-count)))
+
 (defroutes app
-  (ANY "/test"  []  (resource)))
+  (ANY "/apple/:pub-date" [pub-date] (apple pub-date))
+  (ANY "/stats" [] (stats)))
 
 (def handler 
   (-> app 
+      (wrap-reload)
       (wrap-params))) 
 
 (defn -main
@@ -23,5 +41,5 @@
   (timbre/set-config! [:shared-appender-config :spit-filename] (str (env :data-dir) "/patalyze.log"))
 
   (nrepl/start-server :bind "0.0.0.0" :port 42042)
-  (run-jetty (wrap-reload  #'handler)  {:port 3000 :join? false})
+  (run-jetty #'handler {:port 3000 :join? false})
   (println "nREPL Server started on port 42042")) ; :handler (default-handler lighttable-ops)))
