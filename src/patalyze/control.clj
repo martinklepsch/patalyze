7408daf4c5e2ef8140f75e12e0ac94b0b97af200
Martin Klepsch <martinklepsch@googlemail.com>
2014-10-05 13:59:42 +0200

    add draft for function that starts complete indexing process properly paralellized

--- src/patalyze/control.clj
+++ src/patalyze/control.clj
@@ -2,6 +2,8 @@
   (:require [patalyze.retrieval :refer [status where get-archive]]
             [patalyze.parser    :refer [parse]]
             [patalyze.storage   :refer [cache upload-cached download load-cached]]
+            [environ.core       :refer [env]]
+            [taoensso.timbre         :as timbre :refer (log  trace  debug  info  warn  error)]
             [com.climate.claypoole :as cp]))
 
 (set! *print-length* 100)
@@ -37,9 +39,11 @@
 (comment
   (take 10 (load-cached (first (keys  (where (status) {:cached true})))))
 
-  (defn o [n] (take n (where (status) {:cached true} "2013")))
+  (defn o [n] (take n (where (status) {:on-s3 true :cached true})))
 
-  (map get-archive (o 2))
+  (map download (keys (o 100)))
+
+  (count (o 1000))
 
   (keys (o 2))
 
@@ -53,16 +57,36 @@
 
   (count (where (status) {:on-s3 true :cached false} "2013"))
 
-  (map parse-to-s3 (take 5 (keys (where (status) {:on-s3 false :on-disk true}))))
+  (count (keys (where (status) {:on-disk true})))
 
-  (let [chunks     4
-        to-parse   (keys (where (status) {:on-disk true} "2001"))
-        chunk-size (/ (count to-parse) chunks)]
-    (cp/pmap chunks #(map cache (doall (map parse %))) (partition-all chunk-size to-parse)))
+  (let [processors (cp/ncpus)
+        chunks     (+ 2 processors)
+        to-parse   (take 2 (keys (where (status) {:on-disk true})))
+        n-to-parse (count to-parse)
+        chunk-size (if (zero? (quot n-to-parse chunks)) 1 (quot n-to-parse chunks))
+        rest-chunk (if (< (* chunks chunk-size) n-to-parse) 0 (mod n-to-parse chunk-size))
+        cache-fn   #(do
+                      (cache %2)
+                      (info "Cache Status:" (count (file-seq (clojure.java.io/file (env :data-dir) "cache/applications"))) "/" n-to-parse))]
+
+    (info "Starting Parsing Process")
+    (info (count to-parse) "archives to parse")
+    (info (float (/ n-to-parse chunk-size)) "chunks")
+    (info chunk-size "+" rest-chunk "archives per chunk")
+    (cp/pmap chunks (fn [al] (doall (map-indexed cache-fn (map #(parse %) al))))
+          (partition-all chunk-size to-parse)))
+
+  (first (partition-all 3   (keys (where (status) {:on-disk true} "201"))))
 
   (map parse-to-s3 (take 1 (keys (where (status) {:on-s3 false :on-disk true}))))
 
-  (load-into-db (where (status) {:on-s3 true}))
+  (load-into-atom db (where (status) {:cached true} "2013"))
+
+  (reset! db {:loaded-archives #{} :patents #{}})
+
+  (count (where (status) {:cached true}))
+
+  (/ 347148 12)
 
   (second
           (reduce (fn [[found dups] e]
@@ -87,7 +111,6 @@
     (map #(retrieve-applications %)
          (keys (where (status) {:on-s3 true})))))
 
-  (reset! db #{})
   (count @db)
 
   (where (status) {:on-s3 true} "20021212"))
